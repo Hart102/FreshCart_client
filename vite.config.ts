e0866869406f2353cd1d5bb825576e0b79d94f99
Hart102 <hartongit102@gmail.com>
2024-08-20 16:50:24 -0400

    fixed build bug

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,23 @@
 //   },
 // });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   resolve: {
+//     alias: [{ find: "@", replacement: path.resolve(__dirname, ".", "src") }],
+//   },
+//   plugins: [react()],
+
+//   define: {
+//     "process.env": process.env,
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -24,6 +41,11 @@ export default defineConfig({
     alias: [{ find: "@", replacement: path.resolve(__dirname, ".", "src") }],
   },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Empty external array to include everything
+    },
+  },
 
   define: {
     "process.env": process.env,
